ca60b30bb969a19defbb430b0f0e72d0144ff357
skeough <skeough@logmein.com>
2018-05-01 13:44:51 -0400

    Beginning item interfaces

--- Assets/Assets/Scripts/Models/BaseItem.cs
+++ Assets/Assets/Scripts/Models/BaseItem.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Weapon {
-
+public interface iBaseItem {
+    int value {get; set;}
+    string material {get; set;}
+}
+public interface iMelee {
+    void Strike(string dmgType);
 }
 
-public abstract class Armor {
-    
+public interface iRanged {
+    void Shoot(string projectile);
 }
+
+public interface iStackable {
+
+    int stackTo {get; set;}
+    int storeTo {get; set;}
+
+    void Store();
+    void Stack();
+}
+
+public interface iPileable {
+    int pileTo {get; set;}
+    int storeTo {get; set;}
+}
+
 public class Item {
 
     //TODO: Split out groups of defines into interfaces
